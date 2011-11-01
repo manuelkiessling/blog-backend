5b9d870689453f3447b14817228a5781abb8a306
root <root@lubuntu.(none)>
2011-11-01 18:28:57 +0100

    anhaenge, social sharing buttons

--- wp-content/themes/bueno/single.php
+++ wp-content/themes/bueno/single.php
@@ -12,6 +12,20 @@
 
                     <h1 class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
                     
+                    <div id="socialsharing">
+                        <script src="//platform.twitter.com/widgets.js" type="text/javascript"></script>
+                        <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
+
+                        <g:plusone size="medium" annotation="inline"></g:plusone>
+                        <script type="text/javascript">
+                            (function() {
+                                var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+                                po.src = 'https://apis.google.com/js/plusone.js';
+                                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+                            })();
+                        </script>
+                    </div>
+
                     <p class="date">
                     	<span class="day"><?php the_time('j'); ?></span>
                     	<span class="month"><?php the_time('M'); ?></span>
@@ -28,7 +42,7 @@
                      <script type="text/javascript">
                       var disqus_shortname = 'thelogbookofmanuelkiessling';
                       var disqus_identifier = '<?php the_id() ?>';
-                      var disqus_url = '<?php the_permalink() ?>';
+                      var disqus_url = 'http://manuel.kiessling.net<?php the_permalink() ?>';
                       (function() {
                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
                        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
