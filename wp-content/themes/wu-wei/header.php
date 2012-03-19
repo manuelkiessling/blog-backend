0c21cf7be8f6f2c39b8629e52a6bb5cc57716861
root <root@lubuntu.(none)>
2012-03-19 09:04:49 +0100

    nicer arrow for site title

--- wp-content/themes/wu-wei/header.php
+++ wp-content/themes/wu-wei/header.php
@@ -4,7 +4,7 @@
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
-<title><?php wp_title('⇰', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+<title><?php wp_title('»', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 
 <meta name="description" content="<?php bloginfo('description'); ?>" />
 
