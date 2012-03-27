a51a6a48dc986b1c73f1654cc804e9372620fe44
Manuel Kiessling <manuel@kiessling.net>
2012-03-27 16:56:01 +0200

    streamlined head markup

--- wp-content/themes/wu-wei/header.php
+++ wp-content/themes/wu-wei/header.php
@@ -8,15 +8,12 @@
 
 <meta name="description" content="<?php bloginfo('description'); ?>" />
 
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,800' rel='stylesheet' type='text/css' />
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,800" type="text/css" />
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
 <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-
-<?php wp_head(); ?>
 </head>
 
 <body id="top" class="<?php echo setColourScheme(); ?>">
